e291edd86b18f122e928bb764394e243c921d889
hari prasad <hariprasad9693@gmail.com>
2023-08-03 12:37:19 +0530

    Logging and exception handling files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ML-modular-coding-project
+
+createing an environment with commond 
+conda create -p env python==3.8 -y
+
+activate environment with following commond
+conda activate "C:\Users\user\Documents\ML Projects\ML modular coding\ML-modular-coding-project\env"
+or 
+conda activate env/
+
---
+++ exception.py
@@ -0,0 +1,20 @@
+import os ,sys 
+from flask import Flask
+from src.logger import logging
+from src.exception import CustomeExeption
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    try:
+        raise Exception('we are testing our Exception file')
+    except Exception as e:
+        ML = CustomeExeption(e,sys)
+        logging.info(ML.error_message)
+    
+
+        logging.info("We are testing our logging file")
+        return "Welcome to Engineering Wala Bhiya"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
