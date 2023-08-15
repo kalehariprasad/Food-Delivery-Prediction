0fcfd989e79cfc38e91e70deb74c73744ec61c5b
hari prasad <hariprasad9693@gmail.com>
2023-08-15 18:20:50 +0530

    Model training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,37 +12,92 @@ from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
 
 
 @dataclass
 class ModelTrainerConfig:
-    trained_mode_path=MODEL_FILE_PATH
+    trained_model_path=MODEL_FILE_PATH
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
-    def intiate_model_training(self,train_arry,test_arry):
+
+    def intiate_model_training(self,train_arr,test_arr):
         try:
-            X_train,y_train,X_test,y_test=(train_arry[:,:-1],train_arry[:,-1],test_arry[:,:-1],test_arry[:,-1])
-            models={
+            logging.info("'splitting training and test input data")
+            x_train,y_train,x_test,y_test=(
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+            models = {
                 "SVR":SVR(),
-                "RandomForestRegressor":RandomForestRegressor(),
-                "GradientBoostingRegressor":GradientBoostingRegressor(),
-                "DecisionTreeRegressor":DecisionTreeRegressor()
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                
+                }
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+           
+                },
+                "SVR":{"kernel":['linear', 'rbf',],
+                       
 
-
+                }
+                
             }
-            model_report:dict=model_evaluation(X_train,y_train,X_test,y_test,models)
-            print(model_report)
-            best_model_score=max(sorted(model_report.values()))
-            best_model_name=list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
+        
+            model_report=model_evaluation(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,models=models)
 
+
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
-            logging.info(f"best model for the given model is {best_model}with r2_score of {best_model_score}")
-            save_obj(file_path=self.model_trainer_config.trained_mode_path,obj=best_model)
+            best_model = models[best_model_name]
+            logging.info(f"best model found among models is {best_model}")
+
+            if best_model_score<0.6:
+                raise CustomeExeption("No best model found")
+                
+
+            save_obj(
+                file_path=self.model_trainer_config.trained_model_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(x_test)
+            r2_score_value=r2_score(y_test,predicted)
+            logging.info(f"r2_score for the best models is {r2_score_value}")
+            print(f"best moedel for the proble is {best_model} with r2_score of {r2_score_value}")
+
+
+            return r2_score_value
+            
 
         except Exception as e:
             raise CustomeExeption(e,sys)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,6 +3,7 @@ from src.exception import CustomeExeption
 import os, sys
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_obj(file_path, obj):
     try:
@@ -14,15 +15,33 @@ def save_obj(file_path, obj):
     except Exception as e:
         raise CustomeExeption(e, sys)
     
-def model_evaluation(X_train,y_train,X_test,y_test,models):
+def model_evaluation(X_train,y_train,X_test,y_test,models,):
+    
     try:
+        report = {}
 
-        report={}
-        for model_name, model_instance in models.items():
-            model_instance.fit(X_train, y_train)
-            y_test_pred = model_instance.predict(X_test)
-            model_r2_score = r2_score(y_test, y_test_pred)
-            report[model_name] = model_r2_score
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+           # para=param[list(models.keys())[i]]
+
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train, y_train)
+
+            #model.set_params(**gs.best_params_)
+            #model.fit(X_train, y_train)
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
 
 
